a433567ec00f10021dc1fd4213ee5f6409380646
Marissa Castillo <mmcastillo22@gmail.com>
2020-09-12 13:29:50 -0700

    allow uploading images

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 DEVELOPMENT_ENV  = True
 
 app = Flask(__name__)
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
 @app.route('/results')
-def service():
-    return render_template('results.html')
+def results():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('results.html', files=files)
 
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['image_file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            return redirect(url_for('index'))
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('results'))
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 if __name__ == '__main__':
     app.run(debug=DEVELOPMENT_ENV)
