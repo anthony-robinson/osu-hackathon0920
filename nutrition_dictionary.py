9fc8bdefe333dc22cb4fd5f4191eed1fd019e021
Sam Kinkade <kinkades@oregonstate.edu>
2020-09-13 15:17:07 -0400

    Add nutrition facts

---
+++ nutrition_dictionary.py
@@ -0,0 +1,4 @@
+apple_facts = {"calorie": 52, "fat" : 0.17, "protein" : 0.26, "carbohydrate" : 13.81, "sugars" : 10.39}
+banana_facts = {"calorie" : 89, "fat" : 0.33, "protein" : 1.09, "carbohydrate" : 22.84, "sugars" : 12.23}
+
+nutrition_facts = {"apple":apple_facts, "banana":banana_facts}
